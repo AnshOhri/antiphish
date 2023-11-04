ddec232a4b98368bc6dfbac7435f10e2c2300d53
AnshOhri <anshohri4155@gmail.com>
2023-11-04 13:54:12 +0530

    changes in no page found

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,5 +1,16 @@
-const NoPage = () => {
-	return <h1>404</h1>;
+import React from 'react';
+import './NotFound.css'; // Import your CSS file
+
+const NotFound = () => {
+	return (
+		<div className="not-found-container">
+			<h1 className="not-found-title">404 - Page Not Found</h1>
+			<p className="not-found-message">The page you are looking for does not exist.</p>
+			<a className="not-found-link" href="/">
+				Go to Home Page
+			</a>
+		</div>
+	);
 };
 
-export default NoPage;
+export default NotFound;
